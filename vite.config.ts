845e39e07de88c42647200f19e56220d0e899bf3
Khillo <khalilwarwar2@gmail.com>
2025-04-16 14:05:39 +0300

    move pages to src folder and remove trash files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  root: "./src",
+  publicDir: "../public/",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+  plugins: [tailwindcss()],
+});
