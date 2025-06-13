184e49b2705d96f746146d7b88aacdde0bcbb849
Khillo <khalilwarwar2@gmail.com>
2025-06-13 13:04:39 +0300

    fix background color

--- app/ar/home/layout.tsx
+++ app/ar/home/layout.tsx
@@ -6,7 +6,7 @@ export default function HomeLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div dir="rtl" className="h-screen  w-screen bg-[#275287]">
+    <div dir="rtl" className="min-h-screen  w-screen bg-[#275287]">
       <NavBar />
       <div className="w-full flex justify-center">
         <main className="max-w-6xl w-full">{children}</main>
