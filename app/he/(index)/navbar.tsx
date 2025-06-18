a86d719b2b2d747f6e6be1a6d967e95b5c3647d2
Khillo <khalilwarwar2@gmail.com>
2025-06-18 18:30:18 +0300

    added phone menu

--- app/he/(index)/navbar.tsx
+++ app/he/(index)/navbar.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import LoginModal from "./login";
 import SignUpModal from "./signup";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, MenuIcon } from "lucide-react";
 import Link from "next/link";
 import { LanguageList, translatedPages } from "@/app/Languages";
 import { useRouter } from "next/navigation";
@@ -11,6 +11,7 @@ export default function NavBar() {
   const [loginIsOpen, setLoginIsOpen] = useState(false);
   const [registerIsOpen, setRegisterIsOpen] = useState(false);
   const [languageIsOpen, setLanguageIsOpen] = useState(false);
+  const [isPhoneMenuOpen, setIsPhoneMenuOpen] = useState(false);
   const router = useRouter();
 
   return (
@@ -24,6 +25,80 @@ export default function NavBar() {
                 <a href="#" className="text-4xl font-bold text-indigo-600">
                   EduMate
                 </a>
+              </div>
+            </div>
+
+            <div className="flex items-center sm:hidden">
+              <MenuIcon
+                onClick={() => setIsPhoneMenuOpen(!isPhoneMenuOpen)}
+                className="text-blue-500"
+              />
+              <div
+                hidden={!isPhoneMenuOpen}
+                className="fixed flex justify-center flex-col items-center w-screen top-15 start-0  shadow-inner bg-white"
+              >
+                <button
+                  id="login-btn"
+                  className="px-4 py-2 text-indigo-600 font-medium rounded-md hover:bg-indigo-50 transition"
+                  onClick={() => {
+                    setLoginIsOpen(true);
+                  }}
+                >
+                  התחברות
+                </button>
+                <button
+                  id="signup-btn"
+                  className="px-4 py-2  text-indigo-600 font-medium rounded-md hover:bg-indigo-50 transition"
+                  onClick={() => {
+                    setRegisterIsOpen(true);
+                  }}
+                >
+                  הרשמה
+                </button>
+                <div className="relative" id="language-dropdown-container">
+                  <button
+                    type="button"
+                    id="language-dropdown-btn"
+                    className="flex items-center text-gray-700 px-3 py-2 text-sm font-bold rounded-md hover:bg-gray-100"
+                    onClick={() => {
+                      setLanguageIsOpen(!languageIsOpen);
+                    }}
+                  >
+                    <span id="selected-language">Hebrew</span>
+                    <ChevronDown className="ml-1 text-xs"></ChevronDown>
+                  </button>
+                  {languageIsOpen && (
+                    <div
+                      className="w-screen h-screen top-0 start-0  fixed bg-black opacity-0"
+                      onClick={() => setLanguageIsOpen(false)}
+                    ></div>
+                  )}
+                  <dialog
+                    open={languageIsOpen}
+                    id="language-dropdown"
+                    className="fixed start-0 w-screen rounded-md shadow-lg max-h-80 overflow-y-auto bg-white ring-1 ring-black ring-opacity-5"
+                  >
+                    {LanguageList.filter((language) =>
+                      translatedPages.includes(language.code)
+                    ).map((language) => {
+                      return (
+                        <Link
+                          key={language.code}
+                          href={"#"}
+                          onClick={async (e) => {
+                            e.preventDefault();
+                            setLanguageIsOpen(false);
+                            document.cookie = `lang=${language.code}`;
+                            router.refresh();
+                          }}
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                        >
+                          {language.name + " (" + language.code + ")"}
+                        </Link>
+                      );
+                    })}
+                  </dialog>
+                </div>
               </div>
             </div>
             {/* Language Selector */}
