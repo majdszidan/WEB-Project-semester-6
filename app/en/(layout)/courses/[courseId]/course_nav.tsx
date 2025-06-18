33f5dcded3d553f0074314dbc37dce220f4b4966
Khillo <khalilwarwar2@gmail.com>
2025-06-18 19:26:50 +0300

    fix submit on finish exam

--- app/en/(layout)/courses/[courseId]/course_nav.tsx
+++ app/en/(layout)/courses/[courseId]/course_nav.tsx
@@ -36,7 +36,7 @@ export default function CourseNav({
       </div>
       <button
         onClick={submit}
-        disabled={!canSubmit || quiz != length}
+        disabled={!canSubmit || quiz != 0}
         className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
       >
         <SendHorizonalIcon />
